0baed6eaa2211febf3c444da4d7fb43e310e932d
Robert Link <robert.link@pnnl.gov>
2015-08-19 11:43:56 -0400

    Rename main-ext.cpp -> main-api.cpp
    Also rename coupling-test.ini -> api-example.ini

--- source/main-api.cpp
+++ source/main-api.cpp
@@ -1,8 +1,6 @@
 /*
- *  main.cpp - wrapper entry point
- *  hector
- *
- *  Created by Ben on 9/14/10.
+ *  main-api.cpp - example main() using the API
+ *  
  *
  */
 
@@ -26,7 +24,7 @@
 #include "components/imodel_component.hpp"
 
 using namespace std;
-using namespace Hector; 
+using namespace Hector;
 
 void read_and_set_co2(double tlast,double t, Core &core, istream &sim_gcam_emiss);
 void init_emiss_strm(istream &sim_gcam_emiss);
@@ -39,12 +37,12 @@ void init_emiss_strm(istream &sim_gcam_emiss);
 int main (int argc, char * const argv[]) {
 
     try {
-        
+
         // Create the global log
         Logger& glog = Logger::getGlobalLogger();
         glog.open( string( MODEL_NAME ), true, Logger::DEBUG );
         H_LOG( glog, Logger::NOTICE ) << MODEL_NAME << " wrapper start" << endl;
-        
+
         // Parse the main configuration file
         if( argc > 1 ) {
             if( ifstream( argv[1] ) ) {
@@ -57,16 +55,16 @@ int main (int argc, char * const argv[]) {
             H_LOG( glog, Logger::SEVERE ) << "No configuration filename!" << endl;
             H_THROW( "Usage: <program> <config file name>" )
         }
-        
+
         // Initialize the core and send input data to it
         H_LOG( glog, Logger::NOTICE ) << "Creating and initializing the core." << endl;
         Core core;
         core.init();
-        
+
         H_LOG( glog, Logger::NOTICE ) << "Setting data in the core." << endl;
         INIToCoreReader coreParser( &core );
         coreParser.parse( argv[1] );
-        
+
         // Create visitors
         H_LOG( glog, Logger::NOTICE ) << "Adding visitors to the core." << endl;
         CSVOutputVisitor csvOutputVisitor( string( OUTPUT_DIRECTORY ) + "output.csv"  );
@@ -78,20 +76,20 @@ int main (int argc, char * const argv[]) {
             outfilename = string(OUTPUT_DIRECTORY)+"outputstream.csv";
         else
             outfilename = string(OUTPUT_DIRECTORY)+"outputstream_"+runname+".csv";
-        ofstream outputStream(outfilename.c_str()); 
+        ofstream outputStream(outfilename.c_str());
         CSVOutputStreamVisitor csvOutputStreamVisitor( outputStream );
         core.addVisitor( &csvOutputStreamVisitor );
 
         H_LOG(glog, Logger::NOTICE) << "Calling prepareToRun()\n";
         core.prepareToRun();
-        
+
         H_LOG( glog, Logger::NOTICE ) << "Running the core." << endl;
 
         // variables for reading CO2 emissions to pass into the core as it's running.
         double tlast = core.getStartDate();
         ifstream sim_gcam_emiss("input/emissions/RCP6_emissions.csv");
         init_emiss_strm(sim_gcam_emiss);
-        
+
         for(double t=core.getStartDate()+5.0; t<=core.getEndDate(); t+=5.0) {
             read_and_set_co2(tlast, t, core, sim_gcam_emiss);
             core.run(t);
@@ -119,7 +117,7 @@ int main (int argc, char * const argv[]) {
 
         H_LOG(glog, Logger::NOTICE) << "Shutting down all components.\n";
         core.shutDown();
-        
+
         H_LOG( glog, Logger::NOTICE ) << "Hector wrapper end" << endl;
         glog.close();
     }
@@ -133,13 +131,13 @@ int main (int argc, char * const argv[]) {
     catch( ... ) {
       cerr << "Other exception! " << endl;
     }
-    
+
     return 0;
-} 
+}
 
 void read_and_set_co2(double tstrt, double tend, Core &core, istream &sim_gcam_emiss)
 {
-  
+
     double t;
     std::string line;
     std::vector<std::string> splitvec;
@@ -170,7 +168,7 @@ void read_and_set_co2(double tstrt, double tend, Core &core, istream &sim_gcam_e
             core.sendMessage(M_SETDATA, D_EMISSIONS_CF4,
                              message_data(t, unitval(cf4, U_GG)));
             core.sendMessage(M_SETDATA, D_EMISSIONS_HCF22,
-                             message_data(t, unitval(hcf22, U_GG))); 
+                             message_data(t, unitval(hcf22, U_GG)));
 
             std::cout << "t= " << t << "\n"
                       << "\t\tanthro= " << anthro << "\n"
@@ -183,7 +181,7 @@ void read_and_set_co2(double tstrt, double tend, Core &core, istream &sim_gcam_e
               ;
         }
     } while(t < tend);
-    // when t >=  tend, we exit, leaving tend+1 in the stream to read next time. 
+    // when t >=  tend, we exit, leaving tend+1 in the stream to read next time.
 }
 
 void init_emiss_strm(istream &sim_gcam_emiss)
@@ -191,7 +189,6 @@ void init_emiss_strm(istream &sim_gcam_emiss)
     // skip the 4 lines of header information at the beginning of the file
     std::string junk;
     for(int i=0; i<4; ++i) {
-        getline(sim_gcam_emiss, junk); 
+        getline(sim_gcam_emiss, junk);
     }
 }
-
