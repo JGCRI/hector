92a596e81e0843def17fc288ccd7e772ae686e7d
Alexey Shiklomanov <alexey.shiklomanov@gmail.com>
2019-01-16 15:36:00 -0500

    More improvements to INI parsing code
    - Option (enabled by default) to parse time-series variables into
      data.frames
    - Write INI file in a single `writeLines` call rather than a bunch of
      `write` steps
    - Add unit tests

--- R/read_ini.R
+++ R/read_ini.R
@@ -1,7 +1,11 @@
 #' Read Hector INI file into list
 #'
 #' @param file Path to INI file
-#' @return
+#' @param parse_timeseries Logical. If `TRUE` (default), use
+#'   [parse_timeseries()] to convert time series value names (e.g.
+#'   `"Ftalbedo[1980]"`) to a `data.frame`. If `FALSE`, use the full
+#'   string (with year included) as the key name.
+#' @return Nested list of the target object
 #' @author Alexey Shiklomanov
 #' @examples
 #' rcp45_ini <- read_ini(
@@ -9,8 +13,10 @@
 #' )
 #' rcp45_ini$core$run_name
 #' rcp45_ini$onelineocean$enabled
+#' # This one will be a `data.frame`
+#' rcp45_ini$simpleNbox$Ftalbedo
 #' @export
-read_ini <- function(file) {
+read_ini <- function(file, parse_timeseries = TRUE) {
   full_file <- readLines(file)
 
   # Remove comment lines, which start with `;`
@@ -36,7 +42,12 @@ read_ini <- function(file) {
   value_list <- lapply(raw_values, keyvalue)
   names(value_list) <- header_tags
 
-  value_list
+  # Parse time series and return
+  if (parse_timeseries) {
+    lapply(value_list, parse_timeseries)
+  } else {
+    value_list
+  }
 }
 
 #' Write R list to Hector INI file
@@ -57,19 +68,26 @@ read_ini <- function(file) {
 #' @export
 write_ini <- function(ini_list, file) {
   stopifnot(!is.null(names(ini_list)))
-  write("; Config file for hector model", file)
+  string <- "; Config file for hector model"
   for (tag in names(ini_list)) {
-    write(sprintf("[%s]", tag), file, append = TRUE)
+    string <- c(string, sprintf("[%s]", tag))
     sub_list <- ini_list[[tag]]
     for (key in names(sub_list)) {
-      write(sprintf("%s = %s", key, sub_list[[key]]), file, append = TRUE)
+      value <- sub_list[[key]]
+      if (is.data.frame(value)) {
+        string <- c(string,
+                    sprintf("%s[%d] = %s", key, value[["date"]], value[[key]]))
+      } else {
+        string <- c(string, sprintf("%s = %s", key, value))
+      }
     }
   }
+  writeLines(string, file)
   invisible(ini_list)
 }
 
 #' Convert a vector of strings of the form `"name=value"` to a named R
-#' list
+#' list.
 #'
 #' @param x Character vector of the form `"name=value"`
 #' @return Named list
@@ -94,4 +112,35 @@ try_numeric <- function(x) {
     as.numeric(x),
     warning = function(e) x
   )
+}
+
+#' From a list of values, convert the ones that are time series
+#' (e.g.`"Ftalbedo[1980]"`) to a `data.frame`. Leave scalar and
+#' character values (including _paths_ to time series data) as is.
+#'
+#' @param value_list Named list of values from a particular section
+#' @return `value_list`, but with all time series combined into
+#'   `data.frame`s and moved to the end of the list.
+#' @author Alexey Shiklomanov
+parse_timeseries <- function(value_list) {
+  vnames <- names(value_list)
+  is_ts <- grepl("\\[.*\\]", vnames)
+  scalars <- value_list[!is_ts]
+  tss <- value_list[is_ts]
+  ts_names <- gsub("\\[[[:digit:]]+\\]", "", names(tss))
+  ts_years <- as.numeric(gsub(".*\\[([[:digit:]]+)\\.*]", "\\1", names(tss)))
+  ts_groups <- split(tss, ts_names)
+  ts_years_g <- split(ts_years, ts_names)
+
+  ts <- mapply(
+    function(values, years, value_col) {
+      dat <- data.frame(x = years, y = unlist(values), row.names = NULL)
+      colnames(dat) <- c("date", value_col)
+      dat
+    },
+    ts_groups, ts_years_g, as.list(names(ts_groups)),
+    SIMPLIFY = FALSE
+  )
+
+  c(scalars, ts)
 }
---
+++ tests/testthat/test_ini.R
@@ -0,0 +1,44 @@
+context("Reading and writing INI files")
+
+tempdir <- tempfile()
+dir.create(tempdir, showWarnings = FALSE)
+teardown({
+  unlink(tempdir, recursive = TRUE)
+})
+
+test_that("RCP4.5 is read and written correctly", {
+  correct_names <- c(
+    "core",
+    "onelineocean", "ocean",
+    "simpleNbox", "carbon-cycle-solver",
+    "so2", "CH4", "OH", "ozone", "N2O",
+    "forcing", "temperature",
+    "bc", "oc",
+    "CF4_halocarbon", "C2F6_halocarbon",
+    "HFC23_halocarbon", "HFC32_halocarbon",
+    "HFC4310_halocarbon", "HFC125_halocarbon", "HFC134a_halocarbon",
+    "HFC143a_halocarbon", "HFC227ea_halocarbon", "HFC245fa_halocarbon",
+    "SF6_halocarbon", "CFC11_halocarbon", "CFC12_halocarbon",
+    "CFC113_halocarbon", "CFC114_halocarbon", "CFC115_halocarbon",
+    "CCl4_halocarbon", "CH3CCl3_halocarbon", "halon1211_halocarbon",
+    "halon1301_halocarbon", "halon2402_halocarbon", "HCF22_halocarbon",
+    "HCF141b_halocarbon", "HCF142b_halocarbon", "CH3Cl_halocarbon",
+    "CH3Br_halocarbon")
+  rcp45_ini <- read_ini(
+    system.file("input", "hector_rcp45.ini", package = "hector")
+  )
+  expect_named(rcp45_ini, correct_names)
+  expect_equal(rcp45_ini[[c("core", "run_name")]], "rcp45")
+  expect_equal(rcp45_ini[[c("onelineocean", "enabled")]], 0)
+  ftalbedo <- rcp45_ini[[c("simpleNbox", "Ftalbedo")]]
+  expect_s3_class(ftalbedo, "data.frame")
+  expect_named(ftalbedo, c("date", "Ftalbedo"))
+  expect_equal(nrow(ftalbedo), 2)
+
+  rcp45_testfile <- file.path(tempdir, "rcp45.ini")
+  write_ini(rcp45_ini, rcp45_testfile)
+  expect_true(file.exists(rcp45_testfile))
+  expect_equal(grep("^\\[core\\]", readLines(rcp45_testfile)), 2)
+  rcp45_test <- read_ini(rcp45_testfile)
+  expect_identical(rcp45_ini, rcp45_test)
+})
