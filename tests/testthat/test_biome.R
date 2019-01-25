7ec41ffa750f1621a4a00db95cb31c4905158be1
Alexey Shiklomanov <alexey.shiklomanov@gmail.com>
2019-01-25 12:22:45 -0500

    Fix `warmingfactor` comments in INI files. Add `warmingfactor` test.

--- tests/testthat/test_biome.R
+++ tests/testthat/test_biome.R
@@ -4,19 +4,25 @@ test_that("Hector runs with multiple biomes.", {
 
   skip_if_not_installed("hectortools")
 
-  vars <- c(
-    ATMOSPHERIC_CO2(),
-    RF_TOTAL(),
-    GLOBAL_TEMP()
-  )
+  quickrun <- function(ini_list, name) {
+    ini_file <- tempfile()
+    on.exit(file.remove(ini_file), add = TRUE)
+    hectortools::write_ini(ini_list, ini_file)
+    core <- newcore(ini_file, name = name, suppresslogging = TRUE)
+    invisible(run(core))
+    on.exit(shutdown(core), add = TRUE)
+    dates <- seq(2000, 2100)
+    vars <- c(
+      ATMOSPHERIC_CO2(),
+      RF_TOTAL(),
+      GLOBAL_TEMP()
+    )
+    fetchvars(core, dates, vars)
+  }
 
-  dates <- seq(2000, 2100)
   rcp45_file <- system.file("input", "hector_rcp45.ini", package = "hector")
   rcp45 <- hectortools::read_ini(rcp45_file)
-  rcp45_core <- newcore(rcp45_file, name = "default")
-  invisible(run(rcp45_core))
-  rcp45_result <- fetchvars(rcp45_core, dates, vars)
-  invisible(shutdown(rcp45_core))
+  rcp45_result <- quickrun(rcp45, "default")
 
   # Set biome-specific variables
   biome <- modifyList(rcp45, list(simpleNbox = list(
@@ -39,15 +45,21 @@ test_that("Hector runs with multiple biomes.", {
     boreal.q10_rh = 2.0,
     tropical.q10_rh = 2.0
   )))
-  biome_file <- tempfile()
-  hectortools::write_ini(biome, biome_file)
-  teardown(file.remove(biome_file))
-  core <- newcore(biome_file, name = "biome")
-  invisible(run(core))
-  result <- fetchvars(core, dates, vars = vars)
-  invisible(shutdown(core))
+  biome_result <- quickrun(biome, "biome")
 
-  result_diff <- rcp45_result$value - result$value
+  result_diff <- rcp45_result$value - biome_result$value
   diff_summary <- tapply(result_diff, rcp45_result$variable, sum)
   expect_true(all(abs(diff_summary) > 0))
+
+  # Add the warming tag
+  warm_biome <- modifyList(biome, list(simpleNbox = list(
+    boreal.warmingfactor = 2.5,
+    tropical.warmingfactor = 1.0
+  )))
+  warm_biome_result <- quickrun(warm_biome, "warm_biome")
+  default_tgav <- rcp45_result[rcp45_result[["variable"]] == "Tgav",
+                               "value"]
+  warm_tgav <- warm_biome_result[warm_biome_result[["variable"]] == "Tgav",
+                                 "value"]
+  expect_true(mean(default_tgav) < mean(warm_tgav))
 })
