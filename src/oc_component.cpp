30e854a9f7c10daaed720979e593c27991afd560
mnichol3 <nicholson.matt94@gmail.com>
2019-11-19 17:30:58 -0500

    Whitespace reformatting c/o RStudio

--- src/oc_component.cpp
+++ src/oc_component.cpp
@@ -18,7 +18,7 @@
 #include "avisitor.hpp"
 
 namespace Hector {
-  
+
 using namespace std;
 
 //------------------------------------------------------------------------------
@@ -39,7 +39,7 @@ OrganicCarbonComponent::~OrganicCarbonComponent() {
 // documentation is inherited
 string OrganicCarbonComponent::getComponentName() const {
     const string name = ORGANIC_CARBON_COMPONENT_NAME;
-    
+
     return name;
 }
 
@@ -62,20 +62,20 @@ unitval OrganicCarbonComponent::sendMessage( const std::string& message,
                                             const message_data info ) throw ( h_exception )
 {
     unitval returnval;
-    
+
     if( message==M_GETDATA ) {          //! Caller is requesting data
         return getData( datum, info.date );
-        
+
     } else if( message==M_SETDATA ) {   //! Caller is requesting to set data
         //TODO: call setData below
         //TODO: change core so that parsing is routed through sendMessage
         //TODO: make setData private
         setData(datum, info);
-        
+
     } else {                        //! We don't handle any other messages
         H_THROW( "Caller sent unknown message: "+message );
     }
-    
+
     return returnval;
 }
 
@@ -85,7 +85,7 @@ void OrganicCarbonComponent::setData( const string& varName,
                                       const message_data& data ) throw ( h_exception )
 {
     H_LOG( logger, Logger::DEBUG ) << "Setting " << varName << "[" << data.date << "]=" << data.value_str << std::endl;
-    
+
     try {
         if( varName ==  D_EMISSIONS_OC ) {
             H_ASSERT( data.date != Core::undefinedIndex(), "date required" );
@@ -102,7 +102,7 @@ void OrganicCarbonComponent::setData( const string& varName,
 //------------------------------------------------------------------------------
 // documentation is inherited
 void OrganicCarbonComponent::prepareToRun() throw ( h_exception ) {
-    
+
     H_LOG( logger, Logger::DEBUG ) << "prepareToRun " << std::endl;
     oldDate = core->getStartDate();
 }
@@ -118,17 +118,17 @@ void OrganicCarbonComponent::run( const double runToDate ) throw ( h_exception )
 // documentation is inherited
 unitval OrganicCarbonComponent::getData( const std::string& varName,
                                         const double date ) throw ( h_exception ) {
-    
+
     unitval returnval;
-    
+
     H_ASSERT( date != Core::undefinedIndex(), "Date required for oc_component" );
-    
+
     if( varName ==  D_EMISSIONS_OC  ) {
         returnval = OC_emissions.get( date );
     } else {
         H_THROW( "Caller is requesting unknown variable: " + varName );
     }
-    
+
     return returnval;
 }
 
