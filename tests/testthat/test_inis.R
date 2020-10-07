c09a6aa7967d089d171e6322ee676f288201885f
kdorheim <kalyn.dorheim@pnnl.gov>
2020-10-07 10:18:21 -0400

    Clean up and rename test that scripts, the ini test checks to make sure that all of the ini files in the input folder can be used to set up a Hector core.

---
+++ tests/testthat/test_inis.R
@@ -0,0 +1,15 @@
+context("Test inis")
+
+# Make sure that all of the ini files can generate an actual hector core.
+test_that('All ini files can be used to set up a hector core', {
+
+    ini_list <- list.files(system.file(package = 'hector', 'input'), pattern = 'ini')
+
+    for(ini in ini_list){
+
+        hc <- isactive(newcore(system.file(package = 'hector', file.path('input', ini))))
+        expect_true(hc)
+
+    }
+
+})
