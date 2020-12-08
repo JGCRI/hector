989f4f5e8d344f1fe5288761980fc339e863dc52
crvernon <chrisrvernon@gmail.com>
2020-12-08 12:47:50 -0500

    test for coverage paths

--- tests/testthat/test_wrapper.R
+++ tests/testthat/test_wrapper.R
@@ -43,7 +43,7 @@ test_that('Basic hcore functionality works', {
     expect_false(isactive(hc))
 })
 
-
+print(dir.exists('/logs'))
 test_that('Write out logs', {
 
     ## Turn logging ON for one test and confirm it runs (see GitHub issues #372 and #381)
@@ -54,12 +54,22 @@ test_that('Write out logs', {
     # file path to the current directory where the package is stored
     run_dir <- dirname(here::here())
 
-    # check for Unix file system otherwise assume Windows
-    if (.Platform$OS.type == 'unix') {
-        log_dir <- file.path(run_dir, 'hector', 'check', 'hector.Rcheck', 'tests', 'testthat', 'logs')
+    # check for running tests locally; otherwise switch to CI paths
+    local_dir <- file.path(run_dir, 'hector', 'tests', 'testthat', 'logs')
+    if (dir.exists(local_dir)) {
+        log_dir <- local_dir
     } else {
-        log_dir <- file.path(run_dir, 'hector', 'check', 'hector.Rcheck', 'tests_i386', 'testthat', 'logs')
+
+        # check for Unix file system otherwise assume Windows
+        if (.Platform$OS.type == 'unix') {
+            log_dir <- file.path(run_dir, 'hector', 'check', 'hector.Rcheck', 'tests', 'testthat', 'logs')
+        } else {
+            log_dir <- file.path(run_dir, 'hector', 'check', 'hector.Rcheck', 'tests_i386', 'testthat', 'logs')
+        }
     }
+
+    print(log_dir)
+    print(list.files(log_dir, recursive = TRUE))
 
     # look for the existence of the `logs` directory for Unix and Windows file systems
     expect_true(dir.exists(log_dir))
