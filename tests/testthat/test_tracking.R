5bec98f1af6d2c4f8b555e2eaacdc2c308d39eae
Ben Bond-Lamberty <bondlamberty@pnnl.gov>
2021-08-05 09:47:38 -0500

    Initial testing structure for @leeyap work

--- tests/testthat/test_tracking.R
+++ tests/testthat/test_tracking.R
@@ -1,14 +1,16 @@
 context("Carbon tracking")
 
-inputdir <- system.file("input", package = "hector")
-testvars <- c(ATMOSPHERIC_CO2(), RF_TOTAL(), GLOBAL_TEMP())
 
+inputdir <- system.file("input", package = "hector")
+tracking_testfile <- file.path(inputdir, "hector_rcp45_tracking.ini")
+notracking_testfile <- file.path(inputdir, "hector_rcp45.ini")
+
+# THINGS WE WANT TO TEST
 
 test_that("No-tracking run produces empty data frame", {
 
     # First Hector core run
-    hc <- newcore(file.path(inputdir, "hector_rcp60.ini"), name = "RCP45", suppresslogging = TRUE)
-# TODO: ensure no tracking
+    hc <- newcore(notracking_testfile, name = "test", suppresslogging = TRUE)
 
     # A run without tracking should return an empty data frame
     run(hc, 2100)
@@ -18,12 +20,26 @@ test_that("No-tracking run produces empty data frame", {
     shutdown(hc)
 })
 
-# tracking data frame has correct dates
-# all year/pool combinations sum to ~1
-# after reset, tracking data frame has correct dates
 
-# OK, how do we handle tracking date changes in R?
-# a set_tracking_date function
-# as a parameter to run() (which then uses setData)
-# as a parameter to core::run()
-# via setvar()   <- this seems the best way?
+test_that("get_tracking_data handles bad inputs", {
+    # error produced if we call it without a proper core
+})
+
+test_that("Tracking run produces correct data", {
+    # tracking data (from get_tracking_data()) is a data.frame
+    # tracking d.f. has particular columns (date, pool, etc)
+    # expect_identical(names(x), c("...", "..."))
+    # tracking data frame has correct dates (not less than trackingDate, not more than end of run, includes all years)
+    # all year/pool combinations sum to ~1
+})
+
+test_that("Tracking works with a core reset", {
+    # (reset works - no error is produced)
+    # after reset to <trackingDate, tracking data frame is empty
+    # after reset to >=trackingDate, tracking data frame has correct dates
+    # run-reset-run, tracking data is correct
+})
+
+test_that("Turning tracking on and off from R works", {
+    # can't do this yet
+})
