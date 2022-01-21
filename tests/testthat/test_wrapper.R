30cb79d3249e8c340279b7d8f61774d13ae4eccd
Ben Bond-Lamberty <bondlamberty@pnnl.gov>
2022-01-21 17:52:51 -0500

    Update to use SSP not RCP

--- tests/testthat/test_wrapper.R
+++ tests/testthat/test_wrapper.R
@@ -182,7 +182,8 @@ test_that("Setting future values does not trigger a reset.", {
 
 
 test_that("Setting parameter values or run date prior to current date does trigger a reset.", {
-  hc <- newcore(file.path(inputdir, "hector_rcp45.ini"), suppresslogging = TRUE)
+  hc <- newcore(file.path(inputdir, "hector_ssp370.ini"), suppresslogging = TRUE)
+  run(hc, 2100)
 
   setvar(hc, 2050:2150, FFI_EMISSIONS(), 0.0, "Pg C/yr")
   expect_false(hc$clean)
