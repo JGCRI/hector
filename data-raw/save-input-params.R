be933a778513ce4a04fa6f378fe60db33e345bdf
leeyap <leeya.pressburger@pnnl.gov>
2022-05-10 14:30:24 -0400

    add comments, roxygen documentation, and modify Rmd

--- data-raw/save-input-params.R
+++ data-raw/save-input-params.R
@@ -1,5 +1,9 @@
+# Since this script will only be run occasionally while actively developing Hector, use
+# devtools::load_all() instead of loading the built Hector package.
 devtools::load_all()
 
+# Read in the input parameter csv file
 input_csv <- read.csv("./data-raw/input_params.csv")
 
-usethis::use_data(input_csv, internal = TRUE, overwrite = TRUE)
+# Save as an object in the package
+usethis::use_data(input_csv, overwrite = TRUE)
