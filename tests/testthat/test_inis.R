8090b6e473bc9599684544083168e93a0bb0b9e1
leeyap <leeya.pressburger@pnnl.gov>
2022-05-16 15:04:08 -0400

    Progress on test 2 - list of character strings, now need to isolate params

--- tests/testthat/test_inis.R
+++ tests/testthat/test_inis.R
@@ -10,8 +10,8 @@ test_that("All ini files can be used to set up a hector core", {
   }
 })
 
-# Check that all parameters found in an ini file match the input_params csv...
-test_that("All ini params are in the input csv", {
+# Check that all parameters found the input_params csv are in the ini files...
+test_that("All csv params are in an ini file...", {
   # Read in ini files
   ini_list <- list.files(system.file(package = "hector", "input"), pattern = "ini")
 
@@ -47,5 +47,42 @@ test_that("All ini params are in the input csv", {
 
 })
 
-# ...and that all parameters in the csv match an ini file
+# ...and that all parameters in the ini files are in the csv
+test_that("All ini parameters are in the input csv", {
 
+  # Read in ini files
+  ini_list <- list.files(system.file(package = "hector", "input"), pattern = "ini")
+
+  for(ini in ini_list){
+    ini_file <- readLines(system.file("input", ini, package = "hector"))
+
+    # Break up file line-by-line
+    split_ini <- strsplit(ini_file, ";")
+
+    # Subset lines with equal sign (identify line as parameter)
+    test <- list()
+    for(n in 1:length(split_ini)) {
+      test[[n]] <- subset(split_ini[[n]], grepl("=", split_ini[[n]]))
+      # If the line in the ini does not contain an = or if it contains more than
+      # one word, it is not a parameter and should be discarded
+      if(length(test[[n]]) == 0 | length(test[[n]]) > 1) test[[n]] <- NA
+    }
+
+    # Get results, drop NAs
+    results <- as.data.frame(t(rbind(test)))
+    results <- subset(results, is.na(results$test) == FALSE)
+
+    # Split up lines before and after the = sign
+    split_params <- list()
+    for(n in 1:nrow(results)) {
+      split_params[[n]] <- strsplit(results$test[[n]], "=")
+    }
+
+    # Get results
+    split_param <- as.data.frame(t(rbind(unlist(split_params))))
+
+
+  }
+
+
+})
