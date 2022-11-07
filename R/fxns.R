1d439b284bf0261d0a1c405037f686fa332e8709
kdorheim <kalyn.dorheim@pnnl.gov>
2022-11-07 07:44:24 -0500

    Add the look up function

---
+++ R/fxns.R
@@ -0,0 +1,18 @@
+#' Get the hector function associated with a specific string
+#'
+#' This function provides the Hector function that returns a the Hector string
+#' @param strstr String name to find the Hector function for
+#' @return Hector function name
+#' @export
+getfxn <- function(str) {
+
+    rows <- match(str, hector::fxntable$string)
+    rslt <- hector::fxntable$fxn[rows]
+    if (any(is.na(rows))) {
+        warning(
+            "Functions for the following are not found: ",
+            paste(str[is.na(rslt)], collapse = ", ")
+        )
+    }
+    as.character(rslt)
+}
