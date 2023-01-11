a270e1c90ef2a2c3fee19b4933c41284532049a5
kdorheim <kalyn.dorheim@pnnl.gov>
2023-01-11 12:10:15 -0500

    fix documentation problems with getfxn and fxntabl

--- R/fxns.R
+++ R/fxns.R
@@ -1,7 +1,7 @@
 #' Get the hector function associated with a specific string
 #'
 #' This function provides the Hector function that returns a the Hector string
-#' @param strstr String name to find the Hector function for
+#' @param str String name to find the Hector function for
 #' @return Hector function name
 #' @export
 #' @seealso getunits
