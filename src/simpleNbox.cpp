8a7d19670b37a0f293b9cdbb5a928117b4a66fb8
kdorheim <kalyn.dorheim@pnnl.gov>
2026-03-13 11:19:56 -0400

    remove atmos C pool

--- src/simpleNbox.cpp
+++ src/simpleNbox.cpp
@@ -246,17 +246,7 @@ void SimpleNbox::setData(const std::string &varName, const message_data &data) {
   }
   try {
     // Initial pools
-    if (varNameParsed == D_ATMOSPHERIC_CO2) {
-      // Hector input files specify initial atmospheric CO2 in terms of
-      // the carbon pool, rather than the CO2 concentration.  Since we
-      // don't have a place to store the initial carbon pool, we convert
-      // it to initial concentration and store that.  It will be converted
-      // back to carbon content when the state variables are set up in
-      // prepareToRun.
-      H_ASSERT(data.date == Core::undefinedIndex(), "date not allowed");
-      H_ASSERT(biome == SNBOX_DEFAULT_BIOME, "atmospheric C must be global");
-      set_c0(data.getUnitval(U_PGC).value(U_PGC) * PGC_TO_PPMVCO2);
-    } else if (varNameParsed == D_PREINDUSTRIAL_CO2) {
+      if (varNameParsed == D_PREINDUSTRIAL_CO2) {
       H_ASSERT(data.date == Core::undefinedIndex(), "date not allowed");
       H_ASSERT(biome == SNBOX_DEFAULT_BIOME,
                "preindustrial CO2 must be global");
